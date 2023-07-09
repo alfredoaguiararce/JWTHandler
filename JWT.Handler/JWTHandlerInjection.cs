ec9c90fd6a8433b6db52bd0f827ec846b8333036
alfredoaguiararce <alfredoaguiararce@gmail.com>
2023-07-08 23:48:27 -0700

    :construction: Project basic configuration

---
+++ JWT.Handler/JWTHandlerInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JWT.Handler
+{
+    public static class JWTHandlerInjection
+    {
+        public static void InjectJWTHandler(this IServiceCollection services)
+        {
+
+        }
+    }
+}
