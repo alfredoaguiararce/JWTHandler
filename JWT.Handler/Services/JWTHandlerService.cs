a6c05a7d05994c274eee5524eb5daeb40c22b535
alfredoaguiararce <alfredoaguiararce@gmail.com>
2023-07-08 23:59:20 -0700

    Create JWTHandlerService.cs

---
+++ JWT.Handler/Services/JWTHandlerService.cs
@@ -0,0 +1,21 @@
+﻿namespace JWT.Handler.Services
+{
+    internal class JWTHandlerService : IJWTHandler
+    {
+        public string GenerateJWT()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IJWTHandler
+    {
+        public string GenerateJWT();
+        public bool ValidateToken();
+    }
+}
