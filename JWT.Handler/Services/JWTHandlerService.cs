f42ea6a80e91ea5e22c58f456500706b7f568627
alfredoaguiararce <alfredoaguiararce@gmail.com>
2023-07-09 23:52:54 -0700

    :construction:  Add basic functionability of GenerateJWT feature
    [ADD] Dependencies

--- JWT.Handler/Services/JWTHandlerService.cs
+++ JWT.Handler/Services/JWTHandlerService.cs
@@ -1,13 +1,32 @@
 ﻿using JWT.Handler.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWT.Handler.Services
 {
     internal class JWTHandlerService : IJWTHandler
     {
-
-        public string GenerateJWT(JWTHandlerConfiguration configuration)
+        public JWTHandlerService()
         {
-            throw new NotImplementedException();
+        }
+
+        public string GenerateJWT(JWTHandlerConfiguration Configuration, Claim[] Claims, double MinutesDuration = 60)
+        {
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.SecretKey));
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: Configuration.Issuer
+                    ,audience: Configuration.Audience
+                    ,claims: Claims
+                    ,expires: DateTime.UtcNow.AddMinutes(MinutesDuration)
+                    ,signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public bool ValidateToken()
@@ -18,7 +37,7 @@ namespace JWT.Handler.Services
 
     public interface IJWTHandler
     {
-        public string GenerateJWT(JWTHandlerConfiguration configuration);
+        string GenerateJWT(JWTHandlerConfiguration Configuration, Claim[] Claims, double MinutesDuration = 60);
         public bool ValidateToken();
     }
 }
